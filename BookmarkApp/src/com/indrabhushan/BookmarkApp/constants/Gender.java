2f3b47f5ca623b6cd95a34e04946ea3d72cc8528
Indrabhushan Jaiswar <indrabhushanjaiswar20@gmail.com>
2022-09-30 19:29:27 +0530

    Added Constants

---
+++ BookmarkApp/src/com/indrabhushan/BookmarkApp/constants/Gender.java
@@ -0,0 +1,10 @@
+package com.indrabhushan.BookmarkApp.constants;
+
+public class Gender {
+	private Gender() {
+	}
+
+	public static final int MALE = 0;
+	public static final int FEMALE = 1;
+	public static final int TRANGENDER = 2;
+}
